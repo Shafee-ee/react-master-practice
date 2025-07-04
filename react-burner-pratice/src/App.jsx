a86b36a5b31bf5edbc18087aee5621c74afecb39
shafee_azeem <shafeeghani@gmail.com>
2025-07-04 21:32:38 +0530

    Initial commit

---
+++ react-burner-pratice/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import React from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-gray-700 text-white'>
+      hello
+    </div>
+  )
+}
+
+export default App
