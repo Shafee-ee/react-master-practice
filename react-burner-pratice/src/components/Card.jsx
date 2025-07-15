f4e3374ae00576106cfc87e2bb6d7a3b6945e2cb
shafee_azeem <shafeeghani@gmail.com>
2025-07-15 23:12:55 +0530

    Add:edit

--- react-burner-pratice/src/components/Card.jsx
+++ react-burner-pratice/src/components/Card.jsx
@@ -1,106 +1,109 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, forwardRef, useImperativeHandle } from 'react';
 import CardList from './CardList';
 import Form from './Form';
 
 const Card = () => {
-    const [errors, setErrors] = useState({});
+    // prep for removal
+    // const [errors, setErrors] = useState({});
 
-    const [name, setName] = useState('');
-    const [age, setAge] = useState('');
-    const [role, setRole] = useState('');
+    // const [name, setName] = useState('');
+    // const [age, setAge] = useState('');
+    // const [role, setRole] = useState('');
     const [users, setUsers] = useState([]);
-
-    const [editIndex, setEditIndex] = useState(null);// track if we are editing
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const newErrors = {};
-
-        //name validation
-        if (!name.trim()) { newErrors.name = "Name is  Required" }
-        else if (name.trim().length <= 3) { newErrors.name = "Name must atleast be more than 3 characters length" }
-
-        //age validation
-        if (!age.trim()) { newErrors.age = "Age is required" } else if (isNaN(Number(age))) {
-            newErrors.age = "Age must be a number"
-        } else if (Number(age) <= 0 || Number(age) > 120) {
-            newErrors.age = "Age should be within 1-120 range"
-        }
-
-        //Role Validation
-        if (!role.trim()) newErrors.role = "Role is required";
-
-
-        const nameExists = users.some((user, i) => i !== editIndex && user.name.toLowerCase() === name.toLowerCase());
-
-
-        if (nameExists) {
-            newErrors.name = "Name already Exists";
-        }
-
-
-        if (Object.keys(newErrors).length > 0) {
-            setErrors(newErrors);
-            return
-        }
-
-        const newUser = { age, name, role };
-
-        if (editIndex !== null) {
-            const updatedUsers = [...users];
-            updatedUsers[editIndex] = newUser;
-            setUsers(updatedUsers)
-        } else {
-            setUsers(prev => [...users, newUser])
-        }
-
-
-        setName('');
-        setAge('');
-        setRole('');
-        setErrors({});
-        setEditIndex(null);
-
-    }
-
-
+    const formRef = useRef();
 
     const handleEdit = (index) => {
-        const userToEdit = users[index];
-        setName(userToEdit.name);
-        setAge(userToEdit.age);
-        setRole(userToEdit.role);
-        setEditIndex(index);
+        formRef.current?.setFormData(index);
     }
 
     const handleDelete = (index) => {
-
-        //Remove the user at the edit selected index
-        const filtered = users.filter((_, i) => i !== index);
-        setUsers(filtered);
-
-        //Reset the form if user being edited is deleted
-        if (editIndex === index) {
-            setName('');
-            setAge('');
-            setRole('');
-            setEditIndex(null);
-        }
-
+        const updatedUsers = users.filter((_, i) => i !== index);
+        setUsers(updatedUsers);
     }
+
+    // const [editIndex, setEditIndex] = useState(null);// track if we are editing
+
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     const newErrors = {};
+
+    //     //name validation
+    //     if (!name.trim()) { newErrors.name = "Name is  Required" }
+    //     else if (name.trim().length <= 3) { newErrors.name = "Name must atleast be more than 3 characters length" }
+
+    //     //age validation
+    //     if (!age.trim()) { newErrors.age = "Age is required" } else if (isNaN(Number(age))) {
+    //         newErrors.age = "Age must be a number"
+    //     } else if (Number(age) <= 0 || Number(age) > 120) {
+    //         newErrors.age = "Age should be within 1-120 range"
+    //     }
+
+    //     //Role Validation
+    //     if (!role.trim()) newErrors.role = "Role is required";
+
+
+    //     const nameExists = users.some((user, i) => i !== editIndex && user.name.toLowerCase() === name.toLowerCase());
+
+
+    //     if (nameExists) {
+    //         newErrors.name = "Name already Exists";
+    //     }
+
+
+    //     if (Object.keys(newErrors).length > 0) {
+    //         setErrors(newErrors);
+    //         return
+    //     }
+
+    //     const newUser = { age, name, role };
+
+    //     if (editIndex !== null) {
+    //         const updatedUsers = [...users];
+    //         updatedUsers[editIndex] = newUser;
+    //         setUsers(updatedUsers)
+    //     } else {
+    //         setUsers(prev => [...users, newUser])
+    //     }
+
+
+    //     setName('');
+    //     setAge('');
+    //     setRole('');
+    //     setErrors({});
+    //     setEditIndex(null);
+
+    // }
+
+    // const handleEdit = (index) => {
+    //     const userToEdit = users[index];
+    //     setName(userToEdit.name);
+    //     setAge(userToEdit.age);
+    //     setRole(userToEdit.role);
+    //     setEditIndex(index);
+    // }
+
+    // const handleDelete = (index) => {
+
+    //     //Remove the user at the edit selected index
+    //     const filtered = users.filter((_, i) => i !== index);
+    //     setUsers(filtered);
+
+    //     //Reset the form if user being edited is deleted
+    //     if (editIndex === index) {
+    //         setName('');
+    //         setAge('');
+    //         setRole('');
+    //         setEditIndex(null);
+    //     }
+
+    // }
 
     return (
         <div>
             <Form
-                name={name}
-                setName={setName}
-                age={age}
-                setAge={setAge}
-                role={role}
-                setRole={setRole}
-                handleSubmit={handleSubmit}
-                errors={errors}
-                isEditing={editIndex !== null}
+                users={users}
+                setUsers={setUsers}
+                ref={formRef}
             />
             <CardList users={users}
                 handleDelete={handleDelete}
--- react-burner-pratice/src/components/Form.jsx
+++ react-burner-pratice/src/components/Form.jsx
@@ -1,6 +1,81 @@
-import React, { useState } from 'react'
+import React, { useState, forwardRef, useImperativeHandle } from 'react'
 
-const Form = ({ name, setName, age, setAge, role, setRole, handleSubmit, errors, isEditing }) => {
+const Form = forwardRef((props, ref) => {
+    const { users, setUsers } = props;
+    const [errors, setErrors] = useState({});
+
+    const [name, setName] = useState('');
+    const [age, setAge] = useState('');
+    const [role, setRole] = useState('');
+    // const [users, setUsers] = useState([]);
+    const [editIndex, setEditIndex] = useState(null);
+    const isEditing = editIndex !== null;
+
+    useImperativeHandle(ref, () => ({
+        setFormData: (index) => {
+            const user = users[index];
+            if (!user) return;
+
+            setName(user.name);
+            setAge(user.age);
+            setRole(user.role);
+            setEditIndex(index);
+        }
+    }))
+
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newErrors = {};
+
+        if (!name.trim()) {
+            newErrors.name = "Name is required";
+        } else if (name.trim().length <= 3) {
+            newErrors.name = "Name must be more than 3 characters"
+        }
+
+        if (!age.trim()) {
+            newErrors.age = "Age is a required field";
+        } else if (Number(age) <= 16 || Number(age) > 120) {
+            newErrors.age = "Your age is not valid";
+        }
+
+        if (!role.trim()) {
+            newErrors.role = "Role is a required field"
+        }
+
+        const nameExists = users.some(
+            (user, i) => i !== editIndex && user.name.toLowerCase() === name.toLowerCase()
+        )
+
+        if (nameExists) {
+            newErrors.name = "Name already exists"
+        }
+
+        if (Object.keys(newErrors).length > 0) {
+            setErrors(newErrors);
+            return;
+        }
+
+        const newUser = { name, age, role };
+
+        if (editIndex !== null) {
+            const updatedUsers = [...users];
+            updatedUsers[editIndex] = newUser;
+            setUsers(updatedUsers)
+        } else {
+            setUsers([...users, newUser])
+        }
+
+        //Reset
+        setName('');
+        setAge('');
+        setRole('');
+        setErrors({});
+        setEditIndex(null)
+
+    }
 
 
     return (
@@ -28,6 +103,6 @@ const Form = ({ name, setName, age, setAge, role, setRole, handleSubmit, errors,
             <button onClick={handleSubmit} className='bg-green-300 px-3 py-2 hover:bg-green-400 w-50 mx-auto mt-5'>{isEditing ? 'Update User' : 'Add User'}</button>
         </form>
     )
-}
+})
 
 export default Form
